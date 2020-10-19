8f32c2ec78ab95ccea617d71150b08d7486eee57
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-20 01:15:03 +0300

    feat: add api services implementations for data models

---
+++ src/NovaPoshta.Client/Services/AddressService.cs
@@ -0,0 +1,82 @@
+﻿using NovaPoshta.Client.Abstractions;
+using NovaPoshta.Client.Abstractions.Connection;
+using NovaPoshta.Client.Abstractions.Services;
+using NovaPoshta.Client.Models;
+using NovaPoshta.Client.Models.Data.Address;
+using NovaPoshta.Client.Models.Props.Address;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NovaPoshta.Client.Services
+{
+    public sealed class AddressService : ApiClient, IAddressService
+    {
+        public AddressService(IApiConnection connection)
+            : base(connection)
+        {
+        }
+
+        public Task<ResponsePayload<DeleteContragentAddressData>> DeleteContragentAddress(Guid addressRef)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<GetAreasData>> GetAreas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<GetCitiesData>> GetCities(Guid cityRef)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<GetSettlementsData>> GetSettlements(Guid areaRef, Guid addressRef, Guid regionRef, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<GetStreetData>> GetStreet(Guid cityRef)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<GetWarehousesData>> GetWarehouses(string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<SaveContragentAddressData>> SaveContragentAddress(Guid counterpartyRef, Guid streetRef, string buildingNumber, ushort flat, string note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<SearchSettlementsData>> SearchSettlements(string cityName, uint limit)
+        {
+            var payload = new RequestPayload<SearchSettlementsProps>
+            {
+                ApiKey = base.ApiConnection.ApiKey,
+                ModelName = "Address",
+                CalledMethod = "searchSettlements",
+                MethodProperties = new SearchSettlementsProps
+                {
+                    CityName = cityName,
+                    Limit = limit
+                }
+            };
+
+            return base.ApiConnection.PostAsync<SearchSettlementsProps, SearchSettlementsData>(payload);
+        }
+
+        public Task<ResponsePayload<SearchSettlementStreetData>> SearchSettlementStreets(Guid settlementRef, string streetName, uint limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponsePayload<UpdateContragentAddressData>> UpdateContragentAddress(Guid counterpartyRef, Guid streetRef, Guid addressRef, string buildingNumber, ushort flat, string note)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NovaPoshta.Client/Services/ExpressWaybillService.cs
@@ -0,0 +1,62 @@
+﻿using NovaPoshta.Client.Abstractions;
+using NovaPoshta.Client.Abstractions.Connection;
+using NovaPoshta.Client.Abstractions.Services;
+using NovaPoshta.Client.Models;
+using NovaPoshta.Client.Models.Data.ExpressWaybill;
+using NovaPoshta.Client.Models.Props.ExpressWaybill;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaPoshta.Client.Services
+{
+    public sealed class ExpressWaybillService : ApiClient, IExpressWaybillService, IModelNameResolver
+    {
+        public ExpressWaybillService(IApiConnection connection)
+            : base(connection)
+        {
+        }
+
+        public string ModelName => "InternetDocument";
+
+        public Task<ResponsePayload<GetArchiveDocumentsByPhoneData>> GetArchieveDocumentList()
+        {
+            var payload = new RequestPayload<GetArchiveDocumentsByPhoneProps>
+            {
+                ApiKey = ApiConnection.ApiKey,
+                ModelName = ModelName,
+                //CalledMethod = "getDocumentList",
+                CalledMethod = "getArchiveDocumentsByPhone",
+                MethodProperties = new GetArchiveDocumentsByPhoneProps
+                {
+                    DateFrom = $"{DateTime.UtcNow.AddDays(-90).Date.ToString("dd.MM.yyyy")} 00:00:00",
+                    DateTo = $"{DateTime.UtcNow.Date.ToString("dd.MM.yyyy")} 00:00:00",
+                    Page = 1,
+                    Limit = 20
+                }
+            };
+
+            return ApiConnection.PostAsync<GetArchiveDocumentsByPhoneProps, GetArchiveDocumentsByPhoneData>(payload);
+        }
+
+        public Task<ResponsePayload<GetIncomingDocumentsByPhoneData>> GetIncomingDocumentList()
+        {
+            var payload = new RequestPayload<GetIncomingDocumentsByPhoneProps>
+            {
+                ApiKey = ApiConnection.ApiKey,
+                ModelName = ModelName,
+                CalledMethod = "getIncomingDocumentsByPhone",
+                MethodProperties = new GetIncomingDocumentsByPhoneProps
+                {
+                    DateFrom = $"{DateTime.UtcNow.AddDays(-60).Date.ToString("dd.MM.yyyy")} 00:00:00",
+                    DateTo = $"{DateTime.UtcNow.AddDays(30).Date.ToString("dd.MM.yyyy")} 00:00:00",
+                    Page = 1,
+                    Limit = 20
+                }
+            };
+
+            return ApiConnection.PostAsync<GetIncomingDocumentsByPhoneProps, GetIncomingDocumentsByPhoneData>(payload);
+        }
+    }
+}
