a72cf6e0112a96a1607994cf738b0b87227ed2c3
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-20 01:14:18 +0300

    feat: add api connection implementations

---
+++ src/NovaPoshta.Client/Connection/ApiConnection.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using NovaPoshta.Client.Abstractions.Connection;
+using NovaPoshta.Client.Models;
+using NovaPoshta.Client.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaPoshta.Client.Connection
+{
+    public sealed class ApiConnection : IApiConnection
+    {
+        public string ApiKey { get; }
+
+        public IConnection Connection { get; }
+
+        public ApiConnection(string apiKey, IConnection connection)
+        {
+            ApiKey = apiKey;
+            Connection = connection;
+        }
+
+        public ApiConnection(string apiKey, Uri baseUrl)
+            : this(apiKey, new Connection(baseUrl))
+        {
+        }
+
+        public ApiConnection(string apiKey, Uri baseUrl, HttpClient httpClient)
+            : this(apiKey, new Connection(baseUrl, httpClient))
+        {
+        }
+
+        public async Task<ResponsePayload<TResponse>> PostAsync<TProps, TResponse>(RequestPayload<TProps> requestPayload)
+        {
+            var response = await Connection.PostAsync(requestPayload)
+                .ConfigureAwait(false);
+
+            //var content = new StringContent(JsonConvert.SerializeObject(requestPayload), Encoding.UTF8, "application/json");
+            //var response = await _httpClient.PostAsync(_urlHost, content)
+            //    .ConfigureAwait(false);
+
+            //// ToDo: Handle if response failed.
+
+            var responsePayload = await response.Content
+                .ReadAsStringAsync()
+                .ConfigureAwait(false);
+
+            try
+            {
+                var res = JsonConvert.DeserializeObject<ResponsePayload<TResponse>>(responsePayload);
+                var errors = res.ToErrorList();
+                var hasError = errors?.Any() ?? false;
+
+                if (hasError)
+                {
+                    // ToDo: How to handle error if 200 Ok response?
+                    // empty or what?
+                }
+
+                return res;
+            }
+            catch (Exception ex)
+            {
+                // ToDo: Worth to track somehow that we had bad request or whatever.
+                return default;
+            }
+        }
+
+        // ToDo: Performance improvement
+        //public Task<ApiResponse<TResponse>> PostAsync<TRequest, TResponse>(RequestPayload<TRequest> requestPayload)
+        //{
+        //    var content = new StringContent(JsonConvert.SerializeObject(requestPayload), Encoding.UTF8, "application/json");
+
+        //    return _httpClient.PostAsync(_urlHost, content)
+        //        .ContinueWith((res) => res.Result.Content.ReadAsStringAsync(), TaskScheduler.Default)
+        //        .Unwrap()
+        //        .ContinueWith((payload) => JsonConvert.DeserializeObject<TResponse>(payload.Result), TaskScheduler.Default);
+        //}
+    }
+}
---
+++ src/NovaPoshta.Client/Connection/Connection.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using NovaPoshta.Client.Abstractions.Connection;
+using NovaPoshta.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaPoshta.Client.Connection
+{
+    public sealed class Connection : IConnection
+    {
+        private readonly HttpClient _httpClient;
+
+        public Uri BaseAddress { get; }
+
+        public Connection(Uri baseUrl)
+            : this(baseUrl, new HttpClient())
+        {
+        }
+
+        public Connection(Uri baseUrl, HttpClient httpClient)
+        {
+            BaseAddress = baseUrl;
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync<TProps>(RequestPayload<TProps> requestPayload)
+        {
+            using (var content = new StringContent(JsonConvert.SerializeObject(requestPayload), Encoding.UTF8, "application/json"))
+            {
+                var response = await _httpClient.PostAsync(BaseAddress, content)
+                    .ConfigureAwait(false);
+
+                // ToDo: HttpCliet should be here
+                return response;
+            };
+        }
+    }
+}
