960c5d1d97461748fa6f54d37f0d743e553a8807
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-21 21:43:01 +0300

    feat: update annotations for express waybill models

--- src/NovaPoshta.Client/Models/Data/ExpressWaybill/GetArchiveDocumentsByPhoneData.cs
+++ src/NovaPoshta.Client/Models/Data/ExpressWaybill/GetArchiveDocumentsByPhoneData.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace NovaPoshta.Client.Models.Data.ExpressWaybill
 {
     public sealed class GetArchiveDocumentsByPhoneData
     {
+        /// <summary>
+        /// List of declared documents for archieved parcels.
+        /// </summary>
         public List<GetDocumentsByPhoneData> Result { get; set; }
     }
 }
--- src/NovaPoshta.Client/Models/Data/ExpressWaybill/GetIncomingDocumentsByPhoneData.cs
+++ src/NovaPoshta.Client/Models/Data/ExpressWaybill/GetIncomingDocumentsByPhoneData.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace NovaPoshta.Client.Models.Data.ExpressWaybill
 {
     public sealed class GetIncomingDocumentsByPhoneData
     {
+        /// <summary>
+        /// List of declared documents for incomming parcels.
+        /// </summary>
         public List<GetDocumentsByPhoneData> Result { get; set; }
     }
 }
